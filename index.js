a8692613b94b120d381b0931652748efc15bef12
Marcus Vinicius <gopokemonvinicius@gmail.com>
2022-05-17 16:25:07 -0300

    tentativa 1

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ const venom = require('venom-bot');
 venom
   .create()
   .then((client) => start(client));
-  venom
-  .create()
-  .then((client) => cpf(client));
-  venom
-  .create()
-  .then((client) => cnpj(client));
-  console.log(start),
-  console.log(cpf),
-  console.log(cnpj)
+
 
 function start(client) {
-  client.onMessage(async (messageInfo) => {
+  client.onMessage((messageInfo) => {
      console.log(messageInfo)
      if (messageInfo.body === 'Ola' || messageInfo.body === 'Oi') {
         console.log('Boa papai!')
@@ -32,40 +24,6 @@ function start(client) {
 
 }
 
-
-function cpf(client) {
-   client.onMessage((messageInfo) => {
-      console.log(messageInfo)
-      if (messageInfo.body === 'cpf' || messageInfo.body === 'CPF') {
-         console.log('Boa papai!')
-         client.sendText(messageInfo.from, 'Ok, agora insira seu cpf ou cnpj para nós verificarmos o seu problema')
-         .then(resp => console.log('Enviado:', resp))
-         .catch(err => console.log(err))
-      } else {
-         console.log('Nâo entendi papai.')
-         client.sendText(cpf.from, 'Não entendi o que você escreveu, reescreva por favor!')
-         .then(resp => console.log('Enviado:', resp))
-         .catch(err => console.log(err))
-      }
-   });
-}
-
-function cnpj(client) {
-   client.onMessage((messageInfo) => {
-      console.log(messageInfo)
-      if (messageInfo.body === 'cnpj' || messageInfo.body === 'CNPJ') {
-         console.log('Boa papai!')
-         client.sendText(messageInfo.from, 'Ok, agora insira seu cnpj ou cpf para nós verificarmos o seu problema')
-         .then(resp => console.log('Enviado:', resp))
-         .catch(err => console.log(err))
-      } else {
-         console.log('Nâo entendi papai.')
-         client.sendText(cnpj.from, 'Não entendi o que você escreveu, reescreva por favor!')
-         .then(resp => console.log('Enviado:', resp))
-         .catch(err => console.log(err))
-      }
-   });
-}
 
 
 
